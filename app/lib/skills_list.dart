cd06e3cc7fb508d9ca4ed5631483d922430e8694
Stefan Borović <sborovic@gmail.com>
2022-01-12 06:30:06 +0100

    Add ListTile styling

--- app/lib/skills_list.dart
+++ app/lib/skills_list.dart
@@ -13,23 +13,32 @@ class _SkillsListState extends State<SkillsList> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      shrinkWrap: true,
-      children: widget.skillNodes.map((e) {
-        if (e.isLeaf == true) {
-          return ListTile(
-            title: Text(e.title),
-          );
-        } else {
-          return Column(children: [
-            ExpansionTile(
-                title: Text(e.title),
-                subtitle: const Text('Trailing expansion arrow icon'),
-                children: <Widget>[
-                  SkillsList(skillNodes: getChildrenOf(e.id)),
-                ])
-          ]);
-        }
-      }).toList(),
-    );
+        shrinkWrap: true,
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: widget.skillNodes.map((e) {
+            if (e.isLeaf == true) {
+              return ListTile(
+                contentPadding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20), 
+                leading: const Icon(Icons.auto_awesome_motion),
+                title: Text(
+                  e.title,
+                  overflow: TextOverflow.fade,
+                  softWrap: false,
+                ),
+                trailing: const Icon(Icons.arrow_forward_ios),
+              );
+            } else {
+              return Column(children: [
+                ExpansionTile(
+                    title: Text(e.title),
+                    //  subtitle: const Text('Trailing expansion arrow icon'),
+                    children: <Widget>[
+                      SkillsList(skillNodes: getChildrenOf(e.id)),
+                    ])
+              ]);
+            }
+          }),
+        ).toList());
   }
 }
